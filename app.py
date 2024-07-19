905b87a808c52ec6bd00904d18a85a7bf13fdbfa
IronForce-Auscent <discordgamer.070910@gmail.com>
2024-07-19 08:41:18 +0800

    Reworked projects endpoint

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import *
+import sqlite3
+
 app = Flask(__name__)
 
+def query_db(db_url, query, parameters): 
+    conn = sqlite3.connect(db_url)
+    cursor = conn.cursor()
+    cursor.execute(query, parameters)
+    data = cursor.fetchall()
+    conn.close()
+    return data
+
 @app.route('/')
+@app.route("/index")
 def index():
     return render_template("index.html")
 
@@ -12,9 +23,17 @@ def projects():
 @app.route("/search", methods=["GET", "POST"])
 def search():
     if request.method == "POST":
-        search = request.form["search"]
-        return redirect(url_for("search", response=search))
+        search = request.form["query"]
+        return render_template("search.html", response=search)
     return render_template("search.html")
+
+@app.route("/test")
+def test():
+    return render_template("test.html")
+
+@app.route("/credits")
+def credits():
+    return render_template("credits.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
